810cab2eb005432b5f04c0c6ea6b3e5bc0bcbf5c
Arvind Kumar <arvind868914@gmail.com>
2022-10-29 20:09:42 +0530

    Skeleton Added.

---
+++ src/main/java/com/logitone/app/javaexercise3/App.java
@@ -0,0 +1,7 @@
+package com.logitone.app.javaexercise3;
+
+public class App {
+    public static void main(String[] args) {
+      
+    }
+}
---
+++ src/main/java/com/logitone/app/javaexercise3/StringFinder.java
@@ -0,0 +1,7 @@
+package com.logitone.app.javaexercise3;
+
+public class StringFinder {
+	 public boolean findString(String searchString, String firstString, String secondString) {
+		 return false;
+	 }
+}
